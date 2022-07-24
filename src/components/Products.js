f1197e91d7bfd36f0d6f2ebd8ef156bbc538d96c
Shaikh Najmul Islam <najmulbge17@gmail.com>
2022-07-24 19:43:42 +0600

    Responsive done

--- src/components/Products.js
+++ src/components/Products.js
@@ -14,7 +14,7 @@ const Products = () => {
         <section className='py-10'>
             <h2 className='text-center text-4xl font-bold uppercase mb-3'>Our Products</h2>
             <p className='text-center mb-8'>Here are the best tech products with affordable price and guarantee.</p>
-            <div className='grid grid-cols-3 gap-5 mx-3 md:mx-20'>
+            <div className='grid grid-cols-1 lg:grid-cols-3 sm:grid-cols-2 gap-5 mx-3 md:mx-20'>
                 {
                     products?.map((p, i) => <ProductCard key={i} product={p} />)
                 }
